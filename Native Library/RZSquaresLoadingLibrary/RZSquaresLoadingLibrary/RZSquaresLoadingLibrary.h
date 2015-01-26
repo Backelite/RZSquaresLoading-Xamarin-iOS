a09cf290fe631a7218001f5e7491fdf31ad667bb
Philippe Bernery <philippe.bernery@backelite.com>
2015-01-26 17:37:22 +0100

    Initial Commit

---
+++ Native Library/RZSquaresLoadingLibrary/RZSquaresLoadingLibrary/RZSquaresLoadingLibrary.h
@@ -0,0 +1,13 @@
+//
+//  RZSquaresLoadingLibrary.h
+//  RZSquaresLoadingLibrary
+//
+//  Created by Philippe Bernery on 26/01/2015.
+//  Copyright (c) 2015 Backelite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RZSquaresLoadingLibrary : NSObject
+
+@end
---
+++ Native Library/RZSquaresLoadingLibrary/RZSquaresLoadingLibrary/RZSquaresLoadingLibrary.m
@@ -0,0 +1,13 @@
+//
+//  RZSquaresLoadingLibrary.m
+//  RZSquaresLoadingLibrary
+//
+//  Created by Philippe Bernery on 26/01/2015.
+//  Copyright (c) 2015 Backelite. All rights reserved.
+//
+
+#import "RZSquaresLoadingLibrary.h"
+
+@implementation RZSquaresLoadingLibrary
+
+@end
